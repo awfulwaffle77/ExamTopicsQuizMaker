be4f45228821de219949856f9aef78f421653159
unknown <heroidorin@gmail.com>
2022-05-05 00:32:43 +0300

    Some bugs at correct answers 🌙

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 4. Repeat for all pages in the exam like a monkey because payment page isn't available
 5. ???
 6. Profit
+
+## Notes
+The implementation is quite poorly tought. It doesn't uses the fact the every card 
+actually has its div. After 2 hours of coding, this actually led to some nasty bug
+where on page 1 there are returned more correct answers (5 instead of 4) and on page
+there are returned less (3 instead of 4). 
+
+The bug on page 10 is due to a community progress bar not existing, but I did not find
+the cause on page 1.
--- main.py
+++ main.py
@@ -3,15 +3,10 @@ import os
 
 RES_DIR = "./res"  # directory of resources
 
-class Card:
-    """ Represents a structure that contains one question with
-        4 answers and a correct answer. 
-
-        To not be confused with a Page, which would hold all the questions
-        and answers on a HTML page.
-    """
+class Page:
+    """ Represnts a structure which holds all the questions and answers on a HTML page. """
     def __init__(self, _questions, _answers, _correct_answer) -> None:
-        self.question = _questions
+        self.questions = _questions
         self.answers = _answers
         self.correct_answer = _correct_answer
 
@@ -20,15 +15,14 @@ class Quiz:
     """ Represents a structure that holds a list of Cards """
     def __init__(self) -> None:
         self.soup_list = self.__init_soup(self.__get_list_of_html())
-        self.cards_list = [] 
+        self.pages_list = [] 
 
         for soup in self.soup_list:
             questions = self.__get_questions(soup)
-            answers = ["This", "that", "Thisc", "Thisd"]
-            correct_answer = "D"
+            answers = self.__get_answers(soup)
+            correct_answer = self.__get_correct_answers(soup)
 
-            # This is incorrect as this creates a page(4 questions, 16 ans etc), not a card
-            self.cards_list.append(Card(questions, answers, correct_answer))
+            self.pages_list.append(Page(questions, answers, correct_answer))
 
 
     def __get_list_of_html(self) -> list:
@@ -57,22 +51,73 @@ class Quiz:
         """ Returns the 4 questions available in this soup as text.
             This returns all the questions on a PAGE.
          """
-        qbodies = soup.find_all("div", attrs={'class': "question-body"})
+        qbodies = soup.find_all("div", attrs={'class': "question-body"})  # question body
 
         questions = []
         for body in qbodies:
             questions.append(body.find("p", attrs={'class': "card-text"}).text)
         return questions
 
+
     def __get_answers(self, soup) -> list:
+        abody = soup.find_all("div", attrs={'class': "question-choices-container"})  # answers body
+
+        answers = []
+        for body in abody:
+            set_of_answers = body.find_all("li", attrs={'class': "multi-choice-item"})  # answers on a card
+            set_of_answers = [_.text for _ in set_of_answers]  # get rid of tags
+            answers.append(set_of_answers)
+        
+        return answers
+
+
+    def __get_correct_answers(self, soup) -> list:
+        """ 
+        Returns a list with the correct answers on the page
+
+        As the "correct answer" stated by ExamTopics is usually wrong,
+        the metric that is used here is to get the most popular (first) 
+        answer from the progress bar (Community vote distribution bar)
+
+        It appears that some questions (ex. #40) do not show the progress
+        bar after clicking reveal, but somehow the tag is still there,
+        even though I cannot see it.
+
+        ! WARN: 
+        Some (many) questions do not have a most voted answer as the
+        correct answer is actually the one selected by ExamTopics.
+        As such, to mitigate the issue, we use the fact that every Card
+        has tag <p class="card-text question-answer bg-light white-text">.
+        If it doesn't have "vote-bar progress-bar bg-primary", we'll
+        default to the primary answer.
+        """
+
+        cabody = soup.find_all("div", attrs={'class': "progress vote-distribution-bar"})  # correct answers body
+
+        canswers = []
+        for body in cabody:
+            # ================ HERE ===============
+            # Modify so it defaults to the answer chosen by ExamTopics to mitigate bug on page 10
+            card_cans = body.find_all("p", attrs={'class': "card-text question-answer bg-light white-text"}) # !!!
+            cans = card_cans.find("div", attrs={'class': "vote-bar progress-bar bg-primary"}).text  # corect answer on a card
+            cans = cans.split(" ")[0]  # answer is usually of type "<letter> <percentage>" and we only want the letter
+            canswers.append(cans)
+            # ======================================
+
+        return canswers
+
 
     def get_questions(self) -> list:
+        """ Momentarily for debugging purposes I suppose.
+
+            Returns all questions (?)
+        """
+
         questions = []
-        for card in self.cards_list:
-            questions.append(card.question)
+        for page in self.pages_list:
+            questions.append(page.questions)
         return questions 
 
 
 if __name__ == "__main__":
     quiz = Quiz()
-    print(quiz.get_questions())
